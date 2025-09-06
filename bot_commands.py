9c808723a1c4c59a6503d72cbf07f1dbb6118444
rubyocorrencia-DRACO <rubyocorrencia@gmail.com>
2025-09-06 11:20:41 -0300

    Delete bot_commands.py

--- bot_commands.py
+++
@@ -1,8 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-
-async def start(update: Update, context: CallbackContext):
-    await update.message.reply_text("Olá! Eu sou o Assistente Virtual.")
-
-async def help_command(update: Update, context: CallbackContext):
-    await update.message.reply_text("Comandos disponíveis:\n/start - Iniciar o bot\n/help - Obter ajuda")
